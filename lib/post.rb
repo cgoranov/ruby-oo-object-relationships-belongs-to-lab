80f6f592c6c2c6bad1a81d56e68d9c1c848ecc6e
Che <cgoranov@gmail.com>
2021-03-18 20:07:54 -0500

    commit tree 93ce6973fca25e1e648dc36878c30d5857e8d64d

---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+
+class Post
+
+    attr_accessor :title
+    attr_reader :author
+
+    def author=(author)
+        @author = author
+    end
+
+end
+
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+
+    attr_accessor :title 
+    attr_reader :artist
+    
+    def artist=(artist)
+        @artist = artist
+    end
+
+    def artist
+        @artist
+    end
+
+end
